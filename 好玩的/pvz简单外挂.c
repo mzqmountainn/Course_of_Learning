fc712defd398d44d6943e84ab4a2989c280d2804
mzq <1845598311@qq.com>
2021-08-25 21:33:14 +0800

    Update pvz简单外挂.c

--- 好玩的/pvz简单外挂.c
+++ 好玩的/pvz简单外挂.c
@@ -7,10 +7,10 @@ int main(void) {
 	DWORD Pid;
 	HANDLE HProcess = 0;
 	while (1) {
-		printf("1  C\n");
+		printf("1.初始化  2.修改太阳\n");
 		scanf("%d", &in);
-		//if (in == 1) {
-			hWnd = FindWindow(NULL, "Plants vs.Zombies");
+		if (in == 1) {
+			hWnd = FindWindow(NULL, "Plants vs. Zombies");
 			if (hWnd != 0) {
 				GetWindowThreadProcessId(hWnd, &Pid);
 				HProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, Pid);
@@ -19,7 +19,30 @@ int main(void) {
 				}
 				else { printf("succeed get process\n"); }
 			}
-		//}
+		}
+		if (in == 2) {
+			if (HProcess == 0) {
+				printf("请先初始化\n");
+			}
+			else
+			{
+				DWORD sun;
+				DWORD sunadd = 0x248B2BE8;
+				printf("你想要多少太阳？\n");
+				scanf("%d", &sun);
+				DWORD res = WriteProcessMemory(HProcess, (LPVOID)sunadd, &sun, 4, 0);
+				if (res)
+				{
+					printf("succeed\n");
+
+				}
+				else
+				{
+					printf("failed\n");
+				}
+
+			}
+		}
 
 	}
 }
