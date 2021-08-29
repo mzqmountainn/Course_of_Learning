4c506b8ceff2ff6fb910ff7650dd2b097874a072
mzq <1845598311@qq.com>
2021-08-29 14:09:01 +0800

    Create .2简单输入输出实例.cpp

---
+++ cppppse/2.开始学习C++/.2简单输入输出实例.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int carots;
+    using namespace std;
+    cout << "Hello World!" << endl;
+    cin >> carots;
+    cout << carots;
+    cin.get();
+    cin.get();
+    return 0;
+}
