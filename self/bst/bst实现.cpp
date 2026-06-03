15762ad75fa2bec95ce0561cee8216e32575cd4c
mzq <53561490+mzqmountainn@users.noreply.github.com>
2026-06-03 15:25:14 +0800

    bst插入实现
    bst插入实现

--- self/bst/bst实现.cpp
+++ self/bst/bst实现.cpp
@@ -9,21 +9,64 @@
 #include <vector>
 #include <cmath>
 #include <string>
+
 using namespace std;
 
-template <typename T, typename Comp = less<T>>
+template<typename T, typename Comp = less<T>>
 
-class BSTree
-{
+class BSTree {
 private:
-    struct Node
-    {
+    struct Node {
         T data_;
-        Node* leftChild;
-        Node* rightchild;
-        Node* root;
-        Node()
+        Node *leftChild;
+        Node *rightChild;
+        Node *root;
+        Node(T data = T()) : data_(data), leftChild(nullptr), rightChild(nullptr) { ; }
     };
 
+    Node *root_;
     Comp comp_;
+public:
+    BSTree(Comp c = Comp()) : root_(nullptr), comp_(c) { ; }
+    //非递归插入
+    void n_insert(T val) {
+        if (root_ == nullptr) {
+            root_ = new Node(val);
+            return;
+        }
+        Node *cur = root_;
+        Node *parent = nullptr;
+        while (cur != nullptr) {
+            //决定是否需要进入左孩子
+            if (comp_(val, cur->data_)) {
+                parent = cur;
+                cur = cur->leftChild;
+            } else if (val == cur->data_) {
+                return;
+            } else if (!comp_(val, cur->data_)) {
+                parent = cur;
+                cur = cur->rightChild;
+            }//判断是否重复
+
+        }
+        cur = new Node(val);
+        //判断插入左孩子还是右孩子
+        if (comp_(val, parent->data_)) {
+            parent->leftChild = cur;
+        } else {
+            parent->rightChild = cur;
+        }
+        cur = nullptr;
+        return;
+    }
 };
+
+int main(void) {
+    BSTree<int> test1;
+    int arr[] = {5, 4, 6, 2, 6, 7, 4, 99};
+    for (auto i: arr) {
+        test1.n_insert(i);
+    }
+    return 0;
+
+}
