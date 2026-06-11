cae111500c1e0c6f56709555f0d3dee97bd61048
mzq <53561490+mzqmountainn@users.noreply.github.com>
2026-06-11 18:28:42 +0800

    bst 递归删除实现

--- self/bst/bst实现.cpp
+++ self/bst/bst实现.cpp
@@ -12,71 +12,89 @@
 
 using namespace std;
 
-template<typename T, typename Comp = less<T> >
+template <typename T, typename Comp = less<T>>
 
-class BSTree {
+class BSTree
+{
 private:
-    struct Node {
+    struct Node
+    {
         T data_;
-        Node *leftChild;
-        Node *rightChild;
-        Node *root;
+        Node* leftChild;
+        Node* rightChild;
+        Node* root;
         Node(T data = T()) : data_(data), leftChild(nullptr), rightChild(nullptr) { ; }
     };
 
-    Node *root_;
+    Node* root_;
     Comp comp_;
 
 public:
     BSTree(Comp c = Comp()) : root_(nullptr), comp_(c) { ; }
     //非递归插入
-    void n_insert(T const &val) {
-        if (root_ == nullptr) {
+    void n_insert(T const& val)
+    {
+        if (root_ == nullptr)
+        {
             root_ = new Node(val);
             return;
         }
-        Node *cur = root_;
-        Node *parent = nullptr;
-        while (cur != nullptr) {
+        Node* cur = root_;
+        Node* parent = nullptr;
+        while (cur != nullptr)
+        {
             //决定是否需要进入左孩子
-            if (comp_(val, cur->data_)) {
+            if (comp_(val, cur->data_))
+            {
                 parent = cur;
                 cur = cur->leftChild;
-            } else if (val == cur->data_) {
+            }
+            else if (val == cur->data_)
+            {
                 return;
-            } else if (!comp_(val, cur->data_)) {
+            }
+            else if (!comp_(val, cur->data_))
+            {
                 parent = cur;
                 cur = cur->rightChild;
             } //判断是否重复
         }
         cur = new Node(val);
         //判断插入左孩子还是右孩子
-        if (comp_(val, parent->data_)) {
+        if (comp_(val, parent->data_))
+        {
             parent->leftChild = cur;
-        } else {
+        }
+        else
+        {
             parent->rightChild = cur;
         }
         cur = nullptr;
         return;
     }
 
-    void n_remove(T const &val);
-    bool n_query(T const &val);
+    void n_remove(T const& val);
+    bool n_query(T const& val);
     /*bst树的前序、中序、后续遍历
      *V 当前节点 L左子节点 R右子节点
      *VLR LVR LRV
      */
-    void preOrder() {
+    void preOrder()
+    {
         cout << "前序遍历:";
         preOrder(root_);
         cout << endl;
     };
-    void inOrder() {
+
+    void inOrder()
+    {
         cout << "中序遍历:";
         inOrder(root_);
         cout << endl;
     };
-    void postOrder() {
+
+    void postOrder()
+    {
         cout << "后序遍历:";
         postOrder(root_);
         cout << endl;
@@ -84,34 +102,47 @@ public:
     //递归实现层级遍历
     void levelOrder(void);
     //递归返回树的高度
-    int high(void) {
+    int high(void)
+    {
         return high(root_);
     }
+
     //递归返回节点数量
-    int number(void) {
+    int number(void)
+    {
         return number(root_);
     }
+
     //递归实现插入元素
-    void insert(const T &val) {
+    void insert(const T& val)
+    {
         root_ = insert(root_, val);
     }
+
     //递归查询元素
-    bool query(const T &val) {
+    bool query(const T& val)
+    {
         return nullptr != query(root_, val);
+    }
+
+    void remove(const T& val)
+    {
+        root_ = remove(root_, val);
     }
 
 private:
     //递归实现 此处本质为深度优先搜索
-    void preOrder(Node *node);
-    void inOrder(Node *node);
-    void postOrder(Node *node);
+    void preOrder(Node* node);
+    void inOrder(Node* node);
+    void postOrder(Node* node);
     //递归实现返回树的深度
-    int high(Node *node);
+    int high(Node* node);
     //递归实现返回树的深度
-    int number(Node *node);
-    void levelOrder(Node *node, int level);
-    Node *insert(Node *node, const T &val);
-    Node *query(Node *node, const T &val);
+    int number(Node* node);
+    void levelOrder(Node* node, int level);
+    Node* insert(Node* node, const T& val);
+    Node* query(Node* node, const T& val);
+    Node* remove(Node* node, const T& val);
 };
 
 /*
@@ -122,21 +153,29 @@ BST树删除节点
 找待删除节点的前驱节点（或者后继节点），用前驱或者后继节点的值把待删除节点的值覆盖
 掉，然后直接删除前驱或者后继节点就可以了
 */
-template<typename T, typename Comp>
-void BSTree<T, Comp>::n_remove(T const &val) {
-    if (root_ == nullptr) {
+template <typename T, typename Comp>
+void BSTree<T, Comp>::n_remove(T const& val)
+{
+    if (root_ == nullptr)
+    {
         return;
     }
     //寻找删除节点
-    Node *cur = root_;
-    Node *parent = nullptr;
-    while (cur != nullptr) {
-        if (cur->data_ == val) {
+    Node* cur = root_;
+    Node* parent = nullptr;
+    while (cur != nullptr)
+    {
+        if (cur->data_ == val)
+        {
             break;
-        } else if (comp_(cur->data_, val)) {
+        }
+        else if (comp_(cur->data_, val))
+        {
             parent = cur;
             cur = cur->rightChild;
-        } else {
+        }
+        else
+        {
             parent = cur;
             cur = cur->leftChild;
         }
@@ -144,14 +183,16 @@ void BSTree<T, Comp>::n_remove(T const &val) {
     //为空说明找不到要删除的元素 结束
     if (cur == nullptr)
         return;
-    Node *target = cur;
+    Node* target = cur;
 
     //接下来开始判断是否属于情况3，判断是否有两个孩子
-    if (cur->leftChild != nullptr && cur->rightChild != nullptr) {
+    if (cur->leftChild != nullptr && cur->rightChild != nullptr)
+    {
         //寻找前驱节点
         parent = cur;
         cur = cur->leftChild;
-        while (cur->rightChild != nullptr) {
+        while (cur->rightChild != nullptr)
+        {
             parent = cur;
             cur = cur->rightChild;
         }
@@ -159,67 +200,92 @@ void BSTree<T, Comp>::n_remove(T const &val) {
         target->data_ = cur->data_;
     }
     //进入删除，首先排除特殊情况：要删除的是root 且root左右孩子中只有一个孩子或者没有
-    if (parent == nullptr && cur == root_) {
-        if (root_->leftChild != nullptr) {
+    if (parent == nullptr && cur == root_)
+    {
+        if (root_->leftChild != nullptr)
+        {
             parent = root_;
             root_ = root_->leftChild;
             delete parent;
             parent = nullptr;
-        } else if ((root_->rightChild != nullptr)) {
+        }
+        else if ((root_->rightChild != nullptr))
+        {
             parent = root_;
             root_ = root_->rightChild;
             delete parent;
             parent = nullptr;
-        } else {
+        }
+        else
+        {
             delete root_;
             root_ = nullptr;
         }
         return;
     }
     //处理正常情况
-    if (parent->leftChild == cur) {
+    if (parent->leftChild == cur)
+    {
         parent->leftChild = (cur->leftChild == nullptr ? cur->rightChild : cur->leftChild);
-    } else if (parent->rightChild == cur) {
+    }
+    else if (parent->rightChild == cur)
+    {
         parent->rightChild = (cur->leftChild == nullptr ? cur->rightChild : cur->leftChild);
     }
     delete cur;
 }
 
-template<typename T, typename Comp>
-bool BSTree<T, Comp>::n_query(T const &val) {
-    Node *cur = root_;
-    while (cur != nullptr) {
-        if (cur->data_ == val) {
+template <typename T, typename Comp>
+bool BSTree<T, Comp>::n_query(T const& val)
+{
+    Node* cur = root_;
+    while (cur != nullptr)
+    {
+        if (cur->data_ == val)
+        {
             return true;
-        } else if (comp_(cur->data_, val)) {
+        }
+        else if (comp_(cur->data_, val))
+        {
             cur = cur->rightChild;
-        } else {
+        }
+        else
+        {
             cur = cur->leftChild;
         }
     }
     return false;
 }
-template<typename T, typename Comp>
-void BSTree<T, Comp>::preOrder(Node *node) {
-    if (node != nullptr) {
+
+template <typename T, typename Comp>
+void BSTree<T, Comp>::preOrder(Node* node)
+{
+    if (node != nullptr)
+    {
         cout << node->data_ << " ";
         preOrder(node->leftChild);
         preOrder(node->rightChild);
     }
     return;
 }
-template<typename T, typename Comp>
-void BSTree<T, Comp>::inOrder(Node *node) {
-    if (node != nullptr) {
+
+template <typename T, typename Comp>
+void BSTree<T, Comp>::inOrder(Node* node)
+{
+    if (node != nullptr)
+    {
         inOrder(node->leftChild);
         cout << node->data_ << " ";
         inOrder(node->rightChild);
     }
     return;
 }
-template<typename T, typename Comp>
-void BSTree<T, Comp>::postOrder(Node *node) {
-    if (node != nullptr) {
+
+template <typename T, typename Comp>
+void BSTree<T, Comp>::postOrder(Node* node)
+{
+    if (node != nullptr)
+    {
         postOrder(node->leftChild);
         postOrder(node->rightChild);
         cout << node->data_ << " ";
@@ -228,9 +294,11 @@ void BSTree<T, Comp>::postOrder(Node *node) {
 }
 
 //递归实现返回树的深度
-template<typename T, typename Comp>
-int BSTree<T, Comp>::high(Node *node) {
-    if (node == nullptr) {
+template <typename T, typename Comp>
+int BSTree<T, Comp>::high(Node* node)
+{
+    if (node == nullptr)
+    {
         return 0;
     }
     int left = high(node->leftChild);
@@ -239,76 +307,154 @@ int BSTree<T, Comp>::high(Node *node) {
 }
 
 //递归返回节点个数
-template<typename T, typename Comp>
-int BSTree<T, Comp>::number(Node *node) {
-    if (node == nullptr) {
+template <typename T, typename Comp>
+int BSTree<T, Comp>::number(Node* node)
+{
+    if (node == nullptr)
+    {
         return 0;
     }
     int left = number(node->leftChild);
     int right = number(node->rightChild);
     return left + right + 1;
 }
+
 //递归进行层级遍历
-template<typename T, typename Comp>
-void BSTree<T, Comp>::levelOrder(void) {
+template <typename T, typename Comp>
+void BSTree<T, Comp>::levelOrder(void)
+{
     int depth = high();
     cout << "层级遍历:";
-    for (int i = 0; i < depth; ++i) {
+    for (int i = 0; i < depth; ++i)
+    {
         levelOrder(root_, i);
     }
     cout << endl;
     return;
 }
+
 //递归进行层级遍历
-template<typename T, typename Comp>
-void BSTree<T, Comp>::levelOrder(Node *node, int level) {
+template <typename T, typename Comp>
+void BSTree<T, Comp>::levelOrder(Node* node, int level)
+{
     if (node == nullptr)
         return;
     //当到达这一层时 才输出 这样保证了只在第n层输出第n层的节点内容
-    if (level == 0) {
+    if (level == 0)
+    {
         cout << node->data_ << " ";
     }
     levelOrder(node->leftChild, level - 1);
     levelOrder(node->rightChild, level - 1);
     return;
 }
+
 //递归实现插入元素
-template<typename T, typename Comp>
-BSTree<T, Comp>::Node *BSTree<T, Comp>::insert(Node *node, const T &val) {
+template <typename T, typename Comp>
+BSTree<T, Comp>::Node* BSTree<T, Comp>::insert(Node* node, const T& val)
+{
     //当node为空时，说明找到了要插入的位置
-    if (node == nullptr) {
+    if (node == nullptr)
+    {
         return new Node(val);
     }
     //当值重复时 不插入
-    if (node->data_ == val) {
+    if (node->data_ == val)
+    {
         return node;
     }
-    if (comp_(node->data_, val)) {
+    if (comp_(node->data_, val))
+    {
         //对子节点进行写入
         node->rightChild = insert(node->rightChild, val);
-    } else {
+    }
+    else
+    {
         node->leftChild = insert(node->leftChild, val);
     }
     return node;
 }
-template<typename T, typename Comp>
-BSTree<T, Comp>::Node *BSTree<T, Comp>::query(Node *node, const T &val) {
-    if (node == nullptr) {
+
+template <typename T, typename Comp>
+BSTree<T, Comp>::Node* BSTree<T, Comp>::query(Node* node, const T& val)
+{
+    if (node == nullptr)
+    {
         return nullptr;
     }
-    if (node->data_ == val) {
+    if (node->data_ == val)
+    {
         return node;
     }
-    if (comp_(node->data_, val)) {
+    if (comp_(node->data_, val))
+    {
         return query(node->rightChild, val);
-    } else {
+    }
+    else
+    {
         return query(node->leftChild, val);
     }
 }
-int main(void) {
+
+template <typename T, typename Comp>
+BSTree<T, Comp>::Node* BSTree<T, Comp>::remove(Node* node, const T& val)
+{
+    if (node == nullptr)
+    {
+        return nullptr;
+    }
+    //找到了
+    if (node->data_ == val)
+    {
+        if (node->leftChild != nullptr && node->rightChild != nullptr)
+        {
+            //情况三：有两个孩子
+            //解决方法：找前驱节点
+            Node* pre = node->leftChild;
+            while (pre->rightChild != nullptr)
+            {
+                pre = pre->rightChild;
+            }
+            node->data_ = pre->data_;
+            //注意 此时原来的val值已经被覆盖 你需要删除的是前驱节点 所以要搜索前驱节点的值
+            node->leftChild = remove(node->leftChild, pre->data_);
+        }
+        else if (node->leftChild != nullptr)
+        {
+            Node* temp = node->leftChild;
+            delete node;
+            return temp;
+        }
+        else if (node->rightChild != nullptr)
+        {
+            Node* temp = node->rightChild;
+            delete node;
+            return temp;
+        }
+        else
+        {
+            delete node;
+            //无子节点
+            return nullptr;
+        }
+    }
+    else if (comp_(node->data_, val))
+    {
+        node->rightChild = remove(node->rightChild, val);
+    }
+    else
+    {
+        node->leftChild = remove(node->leftChild, val);
+    }
+    return node;
+}
+
+int main(void)
+{
     BSTree<int> test1;
     int arr[] = {5, 4, 6, 2, 6, 7, 4, 99};
-    for (auto i: arr) {
+    for (auto i : arr)
+    {
         test1.n_insert(i);
     }
     // cout << test1.n_query(7);
@@ -320,10 +466,12 @@ int main(void) {
     //测试前中后序遍历问题
     BSTree<int> test2;
     int arrr[] = {58, 24, 0, 5, 34, 41, 67, 62, 64, 69, 78};
-    for (auto i: arrr) {
+    for (auto i : arrr)
+    {
         //test2.n_insert(i);
         test2.insert(i);
     }
+    test2.remove(0);
     test2.preOrder();
     test2.inOrder();
     test2.postOrder();
