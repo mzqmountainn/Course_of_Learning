c2e6c87fc1de756bb45ee6905ac74468ab3b4555
mzq <53561490+mzqmountainn@users.noreply.github.com>
2026-08-02 17:36:50 +0800

    avl树

--- self/tree/avl.cpp
+++ self/tree/avl.cpp
@@ -10,45 +10,77 @@
 #include <windows.h>
 #endif
 
-template<typename T>
-class AVLtree {
+template <typename T>
+class AVLtree
+{
 public:
-    AVLtree(void) : root_(nullptr) {
+    AVLtree(void) : root_(nullptr)
+    {
     };
+
+    ~AVLtree()
+    {
+        destroy(root_);
+    }
+
+
     //递归插入
-    void insert(T &value) {
+    void insert(const T& value)
+    {
         root_ = insert(root_, value);
     }
 
+    void remove(const T& value)
+    {
+        root_ = remove(root_, value);
+    }
+
 private:
-    struct Node {
+    struct Node
+    {
         T data_;
-        Node *leftChild_;
-        Node *rightChild_;
+        Node* leftChild_;
+        Node* rightChild_;
         int height_;
-        Node(T data = T()) : data_(data), leftChild_(nullptr), rightChild_(nullptr), height_(1) {
+
+        Node(T data = T()) : data_(data), leftChild_(nullptr), rightChild_(nullptr), height_(1)
+        {
         };
     };
 
-    Node *root_;
+    void destroy(Node* node)
+    {
+        if (node)
+        {
+            destroy(node->leftChild_);
+            destroy(node->rightChild_);
+            delete node;
+        }
+    }
+
+    Node* root_;
 
     //获取高度
-    int height(Node *node) {
+    int height(Node* node)
+    {
         return (node == nullptr) ? 0 : node->height_;
     }
+
     //右旋转操作
-    Node *rightRotate(Node *node);
-    Node *leftRotate(Node *node);
-    Node *leftBalance(Node *node);
-    Node *rightBalance(Node *node);
-    Node *insert(Node *node, T &value);
+    Node* rightRotate(Node* node);
+    Node* leftRotate(Node* node);
+    Node* leftBalance(Node* node);
+    Node* rightBalance(Node* node);
+    Node* insert(Node* node, const T& value);
+    Node* remove(Node* node, const T& value);
 };
 
 //右旋转操作
-template<typename T>
-AVLtree<T>::Node *AVLtree<T>::rightRotate(Node *node) {
+template <typename T>
+AVLtree<T>::Node* AVLtree<T>::rightRotate(Node* node)
+{
     //旋转操作
-    Node *child = node->leftChild_;
+    Node* child = node->leftChild_;
     node->leftChild_ = child->rightChild_;
     child->rightChild_ = node;
     //更新层数
@@ -56,11 +88,13 @@ AVLtree<T>::Node *AVLtree<T>::rightRotate(Node *node) {
     child->height_ = std::max(height(child->leftChild_), height(child->rightChild_)) + 1;
     return child;
 }
+
 //左旋转操作
-template<typename T>
-AVLtree<T>::Node *AVLtree<T>::leftRotate(Node *node) {
+template <typename T>
+AVLtree<T>::Node* AVLtree<T>::leftRotate(Node* node)
+{
     //旋转操作
-    Node *child = node->rightChild_;
+    Node* child = node->rightChild_;
     node->rightChild_ = child->leftChild_;
     child->leftChild_ = node;
     //更新层数
@@ -68,51 +102,71 @@ AVLtree<T>::Node *AVLtree<T>::leftRotate(Node *node) {
     child->height_ = std::max(height(child->leftChild_), height(child->rightChild_)) + 1;
     return child;
 }
+
 //左平衡操作（左-右平衡）
-template<typename T>
-AVLtree<T>::Node *AVLtree<T>::leftBalance(Node *node) {
+template <typename T>
+AVLtree<T>::Node* AVLtree<T>::leftBalance(Node* node)
+{
     node->leftChild_ = leftRotate(node->leftChild_);
     return rightRotate(node);
 }
+
 //右平衡操作（右-左平衡）
-template<typename T>
-AVLtree<T>::Node *AVLtree<T>::rightBalance(Node *node) {
+template <typename T>
+AVLtree<T>::Node* AVLtree<T>::rightBalance(Node* node)
+{
     node->rightChild_ = rightRotate(node->rightChild_);
     return leftRotate(node);
 }
+
 //递归插入私有接口
-template<typename T>
-AVLtree<T>::Node *AVLtree<T>::insert(Node *node, T &value) {
-    if (node == nullptr) {
+template <typename T>
+AVLtree<T>::Node* AVLtree<T>::insert(Node* node, const T& value)
+{
+    if (node == nullptr)
+    {
         return new Node(value);
     }
-    if (node->data_ > value) {
+    if (node->data_ > value)
+    {
         node->leftChild_ = insert(node->leftChild_, value);
-        if (height(node->leftChild_) - height(node->rightChild_) > 1) {
+        if (height(node->leftChild_) - height(node->rightChild_) > 1)
+        {
             //此时失衡
             //判断具体类型
-            if (height(node->leftChild_->leftChild_) >= height(node->leftChild_->rightChild_)) {
+            if (height(node->leftChild_->leftChild_) >= height(node->leftChild_->rightChild_))
+            {
                 //左子树的左孩子高了
                 node = rightRotate(node);
-            } else {
+            }
+            else
+            {
                 //此处为左子树的右孩子高了
                 node = leftBalance(node);
             }
         }
-    } else if (node->data_ < value) {
+    }
+    else if (node->data_ < value)
+    {
         node->rightChild_ = insert(node->rightChild_, value);
-        if (height(node->rightChild_) - height(node->leftChild_) > 1) {
+        if (height(node->rightChild_) - height(node->leftChild_) > 1)
+        {
             //此时失衡
             //判断具体类型
-            if (height(node->rightChild_->rightChild_) >= height(node->rightChild_->leftChild_)) {
+            if (height(node->rightChild_->rightChild_) >= height(node->rightChild_->leftChild_))
+            {
                 //右子树的右孩子高了
                 node = leftRotate(node);
-            } else {
+            }
+            else
+            {
                 //此处为右子树的左孩子高了
                 node = rightBalance(node);
             }
         }
-    } else {
+    }
+    else
+    {
         //此时是有重复的情况 不插入 什么都不做
         ;
     }
@@ -121,7 +175,109 @@ AVLtree<T>::Node *AVLtree<T>::insert(Node *node, T &value) {
 
     return node;
 }
-int main(void) {
+
+template <typename T>
+typename AVLtree<T>::Node* AVLtree<T>::remove(Node* node, const T& value)
+{
+    if (node == nullptr)
+    {
+        return nullptr;
+    }
+    if (node->data_ > value)
+    {
+        node->leftChild_ = remove(node->leftChild_, value);
+        //判断是否失衡
+        if (height(node->rightChild_) - height(node->leftChild_) > 1)
+        {
+            //右子树的右孩子高
+            if (height(node->rightChild_->rightChild_) >= height(node->rightChild_->leftChild_))
+            {
+                node = leftRotate(node);
+            }
+            else
+            {
+                //右子树的左孩子高
+                node = rightBalance(node);
+            }
+        }
+    }
+    else if (node->data_ < value)
+    {
+        node->rightChild_ = remove(node->rightChild_, value);
+        //判断是否失衡
+        if (height(node->leftChild_) - height(node->rightChild_) > 1)
+        {
+            //左子树的左孩子高
+            if (height(node->leftChild_->leftChild_) >= height(node->leftChild_->rightChild_))
+            {
+                node = rightRotate(node);
+            }
+            else
+            {
+                //左子树的右孩子高
+                node = leftBalance(node);
+            }
+        }
+    }
+    else
+    {
+        //找到了
+        //判断有两个孩子的情况
+        if (node->leftChild_ != nullptr && node->rightChild_ != nullptr)
+        {
+            if (height(node->leftChild_) >= height(node->rightChild_))
+            {
+                //寻找前驱节点
+                Node* pre = node->leftChild_;
+                while (pre->rightChild_ != nullptr)
+                {
+                    pre = pre->rightChild_;
+                }
+                node->data_ = pre->data_;
+                node->leftChild_ = remove(node->leftChild_, pre->data_);
+            }
+            else
+            {
+                Node* post = node->rightChild_;
+                //寻找后继节点
+                while (post->leftChild_ != nullptr)
+                {
+                    post = post->leftChild_;
+                }
+                node->data_ = post->data_;
+                node->rightChild_ = remove(node->rightChild_, post->data_);
+            }
+            //有一个孩子
+        }
+        else if (node->leftChild_ != nullptr || node->rightChild_ != nullptr)
+        {
+            if (node->leftChild_ != nullptr)
+            {
+                Node* left = node->leftChild_;
+                delete node;
+                return left;
+            }
+            else
+            {
+                Node* right = node->rightChild_;
+                delete node;
+                return right;
+            }
+        }
+        else
+        {
+            delete node;
+            return nullptr;
+        }
+    }
+    //高度更新
+    node->height_ = std::ranges::max(height(node->leftChild_), height(node->rightChild_)) + 1;
+    return node;
+}
+
+
+int main(void)
+{
 #ifdef _WIN32
     SetConsoleOutputCP(CP_UTF8); // 设置控制台输出为 UTF-8
     SetConsoleCP(CP_UTF8); // 设置控制台输入为 UTF-8
@@ -129,8 +285,13 @@ int main(void) {
     AVLtree<int> test1;
 
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    for (auto it: arr) {
+    for (auto it : arr)
+    {
         test1.insert(it);
     }
+    test1.remove(10);
+    test1.remove(9);
+    test1.remove(8);
+
     return 0;
 }
